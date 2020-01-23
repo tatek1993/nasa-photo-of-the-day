2643c89bfa36a33ba6eb7590ecb4a4c9add02687
Tate Keller <tatek1993@gmail.com>
2020-01-22 18:04:36 -0600

    trying to fix props

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 const Photo = props => {
-    console.log(props);
-}
+    // console.log(props);
+    return(
+        <div className="photoTile">
+            <img alt="{props.title}" className="apod" src="{props.url}"/>
+            <div className="photoInfo">
+                <h2>{props.title}</h2>
+                <h3>{props.date}</h3>
+                <p>{props.explaination}</p>
+                <p>{props.copyright}</p>
+            </div>
+        </div>
+    )
+};
+export default Photo;
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,1 +1,38 @@
 // https://api.nasa.gov/planetary/apod?api_key=004ZxtMALzfSSekpFzXvyZPdQqT3EQh1X3i17V8t
+import React, {useState, useEffect} from "react";
+import Photo from "./Photo";
+import axios from "axios";
+
+const PhotoContainer = props => {
+    console.log(props);
+    const [asop, setAsop] = useState([]);
+    useEffect(() => {
+        axios
+            .get("https://api.nasa.gov/planetary/apod?api_key=004ZxtMALzfSSekpFzXvyZPdQqT3EQh1X3i17V8t")
+            .then(response => {
+                console.log("hello!", response);
+                setAsop(response.data);
+                console.log("me again", asop.title);
+            })
+            .catch(error => {
+                console.log("Data is not returning", error)
+            });
+    }, []);
+   return (
+       <div className="photoOfTheDay">
+            <Photo 
+                key={asop.hdurl}
+                title={asop.title}
+                date={asop.date}
+                url={asop.url}
+                explaination={asop.explaination}
+                copyright={asop.copyright}
+            /> 
+                     
+            {console.log("woof", asop.date)}
+       </div>
+   );
+
+}
+
+export default PhotoContainer;
